b6567dc76cc5cddc1c6f589b71bf05c538c60782
unknown <amirudiin.404@gmail.com>
2021-05-04 22:41:09 +0700

    upload project

---
+++ README.md
@@ -0,0 +1,8 @@
+# UTS-Amirudin-311810362-TI.18.C.1-ActivityIntentFragmentApps
+
+```
+Nama : Amirudin
+NIM : 311810362
+Kelas : TI.18.C.1
+Mata Kuliah : Pemrograman Mobile
+```
---
+++ app/src/main/java/com/example/uts_amirudin_311810362_ti18c1_activityintentfragmentapps/Fragment/WebsiteFragment.java
@@ -0,0 +1,89 @@
+package com.example.uts_amirudin_311810362_ti18c1_activityintentfragmentapps.Fragment;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.uts_amirudin_311810362_ti18c1_activityintentfragmentapps.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WebsiteFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WebsiteFragment extends Fragment implements View.OnClickListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public WebsiteFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WebsiteFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WebsiteFragment newInstance(String param1, String param2) {
+        WebsiteFragment fragment = new WebsiteFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_website, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnNextActivity = view.findViewById(R.id.btn_next);
+        btnNextActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_next) {
+            String url = "https://discord.com";
+            Intent openUrl = new Intent(Intent.ACTION_WEB_SEARCH);
+            openUrl.putExtra(SearchManager.QUERY, url);
+            startActivity(openUrl);
+        }
+    }
+}
